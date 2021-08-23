3596617048d6d3a4a96802ce21f9bd04b72ef287
William Clark Newell <clark@clarknewell.tech>
2021-08-23 11:10:22 -0600

    Blog post is fully rendering
    Blog post is fully rendering, added temp css styles to @sanity/block-content-to-react

--- src/components/BlockContent.js
+++ src/components/BlockContent.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import BaseBlockContent from '@sanity/block-content-to-react';
+import '../styles/typography.css'
 
 const serializers = {
   types: {
     block (props) {
       switch (props.node.style) {
         case 'h1':
-          return <h1 className={}>{props.children}</h1>
+          return <h1 className="headerOne">{props.children}</h1>
 
         case 'h2':
-          return <h2 className={}>{props.children}</h2>
+          return <h2 className="headerTwo">{props.children}</h2>
 
         case 'h3':
-          return <h3 className={}>{props.children}</h3>
+          return <h3 className="headerThree">{props.children}</h3>
 
         case 'h4':
-          return <h4 className={}>{props.children}</h4>
+          return <h4 className="headerFour">{props.children}</h4>
 
         case 'blockquote':
-          return <blockquote className={}>{props.children}</blockquote>
+          return <blockquote className="blockQuote">{props.children}</blockquote>
 
         default:
-          return <p className={}>{props.children}</p>
+          return <p className="textBody">{props.children}</p>
       }
     }
   }
