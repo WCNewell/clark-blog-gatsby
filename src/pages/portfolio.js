a6b3dbad69355def8a11d9449f8da06c83395f22
William Clark Newell <clark@clarknewell.tech>
2021-08-27 13:52:18 -0600

    Update portfolio.js
    Building out links and buttons on portfolio page.

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -18,38 +18,69 @@ const Portfolio = () => (
 					<div>
 						<p>The first iteration of my personal website was created with an early version of Gatsby and a template from HTML5 UP.</p>
 						<h2>Highlighted Projects</h2>
+											{/* Clark Blog */}
+						<a href='/'>
+							<DemoLink>Clark Blog</DemoLink>
+						</a>
+						<RepoLinkButton>	
+						<a href='https://github.com/WCNewell/clark-blog-gatsby'>
+							<Github className='portfolio-icon' alt='github icon' />
+							<p>View the code on Github</p>
+							</a>
+						</RepoLinkButton>
+						<p>Version 3.0 of my personal website presents the following features:</p>	
 						<ul>
-							<RepoLink>	
-								<a href='https://github.com/WCNewell/clark-blog-gatsby'>
-									<h3>Clark Blog</h3>
-									<Github className='portfolio-icon' alt='github icon'/>
-								</a>
-							</RepoLink>
-							<p>Version 3.0 of my personal website presents the following features:</p>	
-							<ul>
-								<li>Built with Gatsby, a frontend framework based on the React library</li>
-								<li>Frontend hosted by Netlify with CI/CD via Github</li>
-								<li>Custom backend and CMS studio built with Sanity.io</li>
-								<li>GraphQL to traverse the Gatsby and Sanity APIs.</li>
-								<li>Sassy CSS and Styled Components</li>
-								<li>Light and dark mode with state management provided by Gatsby and React hooks.</li>
-								<li>Original logo designed with boxy-svg</li>
-								<li>Original blog content</li>
-							</ul>
-							<p>Further Iterations will present the following features:</p>
-							<ul>
-								<li>Accessbility audit and adjustments implmented</li>
-								<li>Unit testing</li>
-								<li>Sorting of blog posts by category</li>
-								<li>Advancents in Sass and CSS</li>
-								<li>A separately hosted fitness blog showcasing different styling</li>
-							</ul>
-							
-							<li>
-								<h3>Clark's Slices</h3>
-								<p>This...</p>	
-							</li>
+							<li>Built with Gatsby, a frontend framework based on the React library</li>
+							<li>Frontend hosted by Netlify with CI/CD via Github</li>
+							<li>Custom backend and CMS studio built with Sanity.io</li>
+							<li>GraphQL to traverse the Gatsby and Sanity APIs.</li>
+							<li>Sassy CSS and Styled Components</li>
+							<li>Light and dark mode with state management provided by Gatsby and React hooks</li>
+							<li>Original logo designed with boxy-svg</li>
+							<li>Original blog content</li>
 						</ul>
+						<p>Further Iterations will present the following features:</p>
+						<ul>
+							<li>Accessbility audit and adjustments implmented</li>
+							<li>Unit testing</li>
+							<li>Sorting of blog posts by category</li>
+							<li>Advancments in Sass and CSS</li>
+							<li>A separately hosted fitness blog showcasing different styling and content</li>
+						</ul>
+										{/* Clark's Slices */}
+						<a href='https://clarks-slices.netlify.app/'>
+							<DemoLink>Clark's Slices</DemoLink>
+						</a>
+						<RepoLinkButton>	
+						<a href='https://github.com/WCNewell/master-gatsby'>
+							<Github className='portfolio-icon' alt='github icon' />
+							<p>View the code on Github</p>
+							</a>
+						</RepoLinkButton>
+						<p>Brief description...</p>
+										{/* Max Hamburger */}
+						<a href='https://wcnewell.github.io/max-hamburger/'>
+							<DemoLink>Max Hamburger</DemoLink>
+						</a>
+						<RepoLinkButton>	
+						<a href='https://github.com/WCNewell/max-hamburger'>
+							<Github className='portfolio-icon' alt='github icon' />
+							<p>View the code on Github</p>
+							</a>
+						</RepoLinkButton>
+						<p>Brief description...</p>
+										{/* Vuetiful Goals */}
+						<a href='https://vuetiful-goals.firebaseapp.com/#/goals'>
+							<DemoLink>Vuetiful Goals</DemoLink>
+						</a>
+						<RepoLinkButton>	
+						<a href='https://github.com/WCNewell/VuetifulGoals-client'>
+							<Github className='portfolio-icon' alt='github icon' />
+							<p>View the code on Github</p>
+							</a>
+						</RepoLinkButton>
+						<p>Brief description...</p>
+
 					</div>
 				</div>
 	</>
@@ -57,8 +88,11 @@ const Portfolio = () => (
 
 export default Portfolio;
 
-const RepoLink = styled.div`
+const RepoLinkButton = styled.button`
 	display: flex;
 	align-content: flex-start;
-	list-style-type: none;
+`
+
+const DemoLink = styled.h3`
+	color: #0b0111;
 `
