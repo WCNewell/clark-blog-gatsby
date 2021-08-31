ac18c40132b0b6c02ecc68e2d31c920bb3f47d8b
William Clark Newell <clark@clarknewell.tech>
2021-08-31 10:14:14 -0600

    Added send button to contact form
    Added send button to contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,25 +5,27 @@ const ContactPage = () => (
     <form method="post" netlify-honeypot="bot-field" data-netlify="true" name="contact">
       <input type="hidden" name="bot-field" />
       <input type="hidden" name="form-name" value="contact" />
-
-        <div>
-          <label>
-            Email
-            <input type="email" name="email" />
-          </label>
+      <div>
+        <label>
+          Email
+          <input type="email" name="email" />
+        </label>
+      </div>
+      <div>
+        <label>
+          Name
+          <input type="text" name="name" />
+        </label>
+      </div>
+      <div>
+        <label>
+          Message
+          <input type="text" name="message" />
+        </label>
+        <div> 
+        <button>Send</button>
         </div>
-        <div>
-          <label>
-            Name
-            <input type="text" name="name" />
-          </label>
-        </div>
-        <div>
-          <label>
-            Message
-            <input type="text" name="message" />
-          </label>
-        </div>
+      </div>
     </form>
   </>
 );
