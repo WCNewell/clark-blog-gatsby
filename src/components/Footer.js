4a51df70caee3572d8f1cdc3f7fe9c9d9dda13f0
William Clark Newell <clark@clarknewell.tech>
2021-09-30 17:57:06 -0600

    Improvements to footer
    Improvements to footer and removing custom scrollbar for now.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import LinkedIn from '../assets/linkedin.inline.svg';
 import Github from '../assets/github.inline.svg';
@@ -14,21 +15,32 @@ const Footer = () => {
                 {` `}
                 <a className="footer-link" aria-label="gatsby" href="https://www.gatsbyjs.org">Gatsby</a>
             </h3>
-            <a href='https://linkedin.com/in/william-clark-newell/' aria-label="linked in">
-                <LinkedIn className='social-icons' alt='linked in icon'/>
-            </a>
-            <a href='https://github.com/WCNewell' aria-label="github">
-                <Github className='social-icons' alt='github icon'/>
-            </a>
-            <a href='https://instagram.com/willcnewell/' aria-label="instagram">
-                <Instagram className='social-icons' alt='instagram icon'/>
-            </a>
-            <a href='https://twitter.com/WCNewell' aria-label="twitter">
-                <Twitter className='social-icons' alt='twitter icon'/>
-            </a>
+            <IconBox>
+              <a href='https://linkedin.com/in/william-clark-newell/' aria-label="linked in">
+                  <LinkedIn className='social-icons' alt='linked in icon'/>
+              </a>
+              <a href='https://github.com/WCNewell' aria-label="github">
+                  <Github className='social-icons' alt='github icon'/>
+              </a>
+              <a href='https://instagram.com/willcnewell/' aria-label="instagram">
+                  <Instagram className='social-icons' alt='instagram icon'/>
+              </a>
+              <a href='https://twitter.com/WCNewell' aria-label="twitter">
+                  <Twitter className='social-icons' alt='twitter icon'/>
+              </a>
+            </IconBox>
         </div>
       </>
   );
 }
 
 export default Footer;
+
+const IconBox = styled.div`
+  width: auto;
+  margin-left: 1.25rem;
+  margin-right: 1.25rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
--- src/components/navigation/burger.js
+++ src/components/navigation/burger.js
@@ -35,7 +35,7 @@ export const StyledBurger = styled.button`
     border: none;
     cursor: pointer;
     padding: 0;
-    z-index: 10;
+    z-index: 2;
     outline: none;
 
     &:focus {
