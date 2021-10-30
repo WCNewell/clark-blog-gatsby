7057bfbd84fd34ca0f6848e481018f9ac41b93c4
William Clark Newell <clark@clarknewell.tech>
2021-10-29 20:37:01 -0600

    Update to blog links
    Update to blog links, title on top and image is also link

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -7,15 +7,15 @@ const SinglePost = ({ post }) => {
   return (
     <>
       <div>
-        <div className="post-image">    
-          <GatsbyImage
-            image={image}
-            alt={post.imageCredit}
-          />
-        </div>
         <span aria-label={post.title}>
           <Link to={`/${post.slug.current}`}>
             <h3 className="dark-link">{post.title}</h3>
+              <div className="post-image">    
+                <GatsbyImage
+                image={image}
+                alt={post.imageCredit}
+                />
+              </div>
           </Link>
           {/* <ul>
             <li className="category">{post.categories.map((category) => category.title).join(' ')}</li>
