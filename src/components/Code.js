3180dfb517e662ef2dbef6d6049f229444c50618
William Clark Newell <clark@clarknewell.tech>
2021-12-10 11:32:48 -0700

    Updates to Code.js
    Working on selecting favored syntax highlights and updated code block scss

--- src/components/Code.js
+++ src/components/Code.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
+import { atelierCaveDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 export default ({ node }) => {
   if (!node || !node.code ) { return null }
   const {language, code} = node
   return (
-    <SyntaxHighlighter language={language || 'text'}>
+    <SyntaxHighlighter className="code-block" style={atelierCaveDark} language={language || 'text'}>
       {code}
     </SyntaxHighlighter>
   )
